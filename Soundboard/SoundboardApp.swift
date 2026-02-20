6d2f84b027842652b90411d9df191908a5408ad8
Charles <zpiao@ascensionacademy.org>
2026-02-20 08:53:43 -0600

    Initial Commit

---
+++ Soundboard/SoundboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundboardApp.swift
+//  Soundboard
+//
+//  Created by Charles Piao on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
